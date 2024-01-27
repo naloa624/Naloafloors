f6655a5efde44b592c376d6b00d9c9c69895040b
naloa624 <157852443+naloa624@users.noreply.github.com>
2024-01-27 04:33:44 -0500

    update vscode

--- submit_form.php
+++ submit_form.php
@@ -1,40 +1,27 @@
 <?php
+$message_sent=false;
 if(isset($_POST['email']) && $_POST['email'] != ''){
-    // Collect form data
+
+if( filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ){
+
+    // submit the form 
     $userName = $_POST['name'];
     $userEmail = $_POST['email'];
     $messageSubject = $_POST['subject'];
     $message = $_POST['message'];
 
-    // Validate email format
-    if (!filter_var($userEmail, FILTER_VALIDATE_EMAIL)) {
-        echo "Invalid email format.";
-        exit();
-    }
-
-    // Email recipient
-    $to = "naloaflooring@gmail.com";
+    $to = "support@naloaflooring.com";
 
     // Email body
     $body = "From: $userName\r\n";
     $body .= "Email: $userEmail\r\n";
     $body .= "Message: $message\r\n";
 
-    // Email headers
-    $headers = "From: $userEmail\r\n";
-    $headers .= "Reply-To: $userEmail\r\n";
-    $headers .= "Content-Type: text/plain; charset=utf-8\r\n";
 
-    // Send email
-    if (mail($to, $messageSubject, $body, $headers)) {
-        echo "<h2>Form Submission Successful</h2>";
-        echo "<p>Thank you for your submission. We will get back to you shortly.</p>";
-    } else {
-        echo "Error sending the email. Please try again later.";
-    }
-} else {
-    // Redirect to the form page if accessed directly
-    header("Location: index.html");
-    exit();
+
+$message_sent=true;
+}
+
+
 }
 ?>
